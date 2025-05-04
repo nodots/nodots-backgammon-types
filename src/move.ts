af58a72b4cb6d123a9d62fae8a3ba867697cfe75
Ken Riley <kenr@nodots.com>
2025-05-03 20:50:36 -0600

    feat: improve move types to handle no-move cases properly

--- src/move.ts
+++ src/move.ts
@@ -1,133 +1,134 @@
-import { BackgammonBoard } from '.';
+import { BackgammonBoard } from '.'
 import {
   BackgammonCheckercontainer,
   BackgammonMoveDestination,
   BackgammonMoveOrigin,
   BackgammonPoint,
-} from './checkercontainer';
-import { BackgammonDieValue } from './dice';
-import { BackgammonMoveDirection } from './game';
-import { BackgammonPlayMoving } from './play';
+} from './checkercontainer'
+import { BackgammonDieValue } from './dice'
+import { BackgammonMoveDirection } from './game'
+import { BackgammonPlayMoving } from './play'
 import {
   BackgammonPlayer,
   BackgammonPlayerActive,
   BackgammonPlayerMoving,
   BackgammonPlayerRolled,
-} from './player';
+} from './player'
 
 export type BackgammonMoveStateKind =
   | 'ready'
   | 'in-progress'
   | 'completed'
-  | 'confirmed';
+  | 'confirmed'
 
 export type BackgammonMoveKind =
   | 'no-move'
   | 'point-to-point'
   | 'reenter'
-  | 'bear-off';
+  | 'bear-off'
 
 export type BackgammonMoveSkeleton = {
-  dieValue: BackgammonDieValue;
-  direction: BackgammonMoveDirection;
-  origin: BackgammonMoveOrigin;
-  destination: BackgammonMoveDestination;
-};
+  dieValue: BackgammonDieValue
+  direction: BackgammonMoveDirection
+  origin: BackgammonMoveOrigin
+  destination: BackgammonMoveDestination
+}
 
 type BaseMove = {
-  id: string;
-  player: BackgammonPlayerActive;
-  stateKind: BackgammonMoveStateKind;
-  dieValue: BackgammonDieValue;
-  possibleMoves: BackgammonMoveSkeleton[];
-  moveKind?: BackgammonMoveKind;
-  isHit?: boolean;
-  origin?: BackgammonMoveOrigin;
-  destination?: BackgammonMoveDestination;
-};
+  id: string
+  player: BackgammonPlayerActive
+  stateKind: BackgammonMoveStateKind
+  dieValue: BackgammonDieValue
+  possibleMoves: BackgammonMoveSkeleton[]
+}
 
-type Move = BaseMove & {
-  stateKind: BackgammonMoveStateKind;
-};
+export type BackgammonMoveReady = BaseMove & {
+  stateKind: 'ready'
+  player: BackgammonPlayerRolled
+  moveKind?: BackgammonMoveKind
+  origin?: BackgammonMoveOrigin
+}
 
-export type BackgammonMoveReady = Move & {
-  stateKind: 'ready';
-  player: BackgammonPlayerRolled;
-  dieValue: BackgammonDieValue;
-};
+export type BackgammonMoveInProgress = BaseMove & {
+  stateKind: 'in-progress'
+  moveKind: BackgammonMoveKind
+  player: BackgammonPlayerMoving
+  origin?: BackgammonMoveOrigin
+  destination?: BackgammonMoveDestination
+}
 
-export type BackgammonMoveInProgress = Move & {
-  stateKind: 'in-progress';
-  moveKind: BackgammonMoveKind;
-  player: BackgammonPlayerMoving;
-  dieValue: BackgammonDieValue;
-  origin: BackgammonMoveOrigin;
-  destination: BackgammonMoveDestination;
-};
+// Split completed moves into two cases
+export type BackgammonMoveCompletedWithMove = BaseMove & {
+  stateKind: 'completed'
+  moveKind: 'point-to-point' | 'reenter' | 'bear-off'
+  origin: BackgammonMoveOrigin
+  destination: BackgammonMoveDestination
+  isHit: boolean
+}
 
-export type BackgammonMoveCompleted = Move & {
-  stateKind: 'completed';
-  moveKind: BackgammonMoveKind;
-  origin: BackgammonMoveOrigin;
-  destination: BackgammonMoveDestination;
-};
+export type BackgammonMoveCompletedNoMove = BaseMove & {
+  stateKind: 'completed'
+  moveKind: 'no-move'
+  origin: undefined
+  destination: undefined
+  isHit: false
+}
 
-export type BackgammonMoveConfirmed = Move & {
-  stateKind: 'confirmed';
-  moveKind: BackgammonMoveKind;
-  origin: BackgammonMoveOrigin;
-  destination: BackgammonMoveDestination;
-};
+export type BackgammonMoveCompleted =
+  | BackgammonMoveCompletedWithMove
+  | BackgammonMoveCompletedNoMove
 
-export type BackgammonMoveNoMove = Move & {
-  stateKind: 'completed';
-  moveKind: 'no-move';
-};
+export type BackgammonMoveConfirmed = BaseMove & {
+  stateKind: 'confirmed'
+  moveKind: BackgammonMoveKind
+  origin: BackgammonMoveOrigin
+  destination: BackgammonMoveDestination
+  isHit: boolean
+}
 
 export type BackgammonMove =
   | BackgammonMoveReady
   | BackgammonMoveInProgress
   | BackgammonMoveCompleted
   | BackgammonMoveConfirmed
-  | BackgammonMoveNoMove;
 
 export type BackgammonMoves =
   | [BackgammonMove, BackgammonMove]
-  | [BackgammonMove, BackgammonMove, BackgammonMove, BackgammonMove];
+  | [BackgammonMove, BackgammonMove, BackgammonMove, BackgammonMove]
 
 export type BackgammonMoveResult = {
-  board: BackgammonBoard;
-  move: BackgammonMoveCompleted;
-};
+  board: BackgammonBoard
+  move: BackgammonMoveCompleted
+}
 
 export type BackgammonMoveDryRunResult = {
-  board: BackgammonBoard;
-  move: BackgammonMoveReady;
-};
+  board: BackgammonBoard
+  move: BackgammonMoveReady
+}
 
 export interface MoveProps {
-  move: BackgammonMove;
-  origin: BackgammonMoveOrigin;
+  move: BackgammonMove
+  origin: BackgammonMoveOrigin
 }
 
 export interface MoveClass {
-  player: BackgammonPlayer;
-  id: string;
-  dieValue: BackgammonDieValue;
-  stateKind: BackgammonMoveStateKind;
-  moveKind: BackgammonMoveKind | undefined;
-  origin: BackgammonCheckercontainer | undefined;
-  destination: BackgammonCheckercontainer | undefined;
+  player: BackgammonPlayer
+  id: string
+  dieValue: BackgammonDieValue
+  stateKind: BackgammonMoveStateKind
+  moveKind: BackgammonMoveKind | undefined
+  origin: BackgammonCheckercontainer | undefined
+  destination: BackgammonCheckercontainer | undefined
 
-  initialize: (props: MoveProps) => BackgammonMove;
+  initialize: (props: MoveProps) => BackgammonMove
   isPointOpen: (
     point: BackgammonPoint,
     player: BackgammonPlayerMoving | BackgammonPlayerRolled
-  ) => boolean;
+  ) => boolean
   move: (
     board: BackgammonBoard,
     move: BackgammonMoveReady,
     isDryRun?: boolean
-  ) => BackgammonMoveResult | BackgammonMoveDryRunResult;
-  confirmMove: (move: BackgammonMoveInProgress) => BackgammonMoveConfirmed;
+  ) => BackgammonMoveResult | BackgammonMoveDryRunResult
+  confirmMove: (move: BackgammonMoveInProgress) => BackgammonMoveConfirmed
 }
