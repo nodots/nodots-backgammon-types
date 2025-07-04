3f4368705fa3d89b099816c3d753dc4276fb31e0
Ken Riley <kenr@nodots.com>
2025-07-04 14:03:40 -0600

    Synching all packages to v3.2.0

--- src/pip.d.ts
+++
@@ -1,4 +0,0 @@
-import { IntegerRange } from './generics';
-export declare const MAX_PIP_COUNT: 167;
-export type BackgammonPips = IntegerRange<0, typeof MAX_PIP_COUNT>;
-//# sourceMappingURL=pip.d.ts.map
--- src/players.d.ts
+++
@@ -1,3 +0,0 @@
-import { PlayerClass } from './player';
-export type Players = [PlayerClass, PlayerClass];
-//# sourceMappingURL=players.d.ts.map
