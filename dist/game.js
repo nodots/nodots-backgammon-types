d02e5a842d59738d9cdf00883f6aac6c17b44926
Ken Riley <kenr@nodots.com>
2025-07-09 01:35:36 -0600

    Remove node_modules and dist from version control, add .gitignore

--- dist/game.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHECKERS_PER_PLAYER = void 0;
-exports.CHECKERS_PER_PLAYER = 15;
--- dist/pip.d.ts
+++
@@ -1,3 +0,0 @@
-import { IntegerRange } from './generics';
-export declare const MAX_PIP_COUNT: 167;
-export type BackgammonPips = IntegerRange<0, typeof MAX_PIP_COUNT>;
